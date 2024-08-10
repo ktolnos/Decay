e7911df0b1e033c49071b28bf1dc10bcd005a13e
eop <ktolnos@mail.com>
2024-08-10 13:08:54 +0300

    Scene update, initial grappling

---
+++ Assets/Scripts/GrapplingArm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class GrapplingArm: MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float force;
+    public State state = State.Idle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Shoot()
+    {
+        rb.simulated = true;
+        rb.velocity = Vector2.zero;
+        rb.AddForce(transform.forward * force, ForceMode2D.Impulse);
+        state = State.Flying;
+    }
+
+    public void Detach()
+    {
+        rb.simulated = false;
+        state = State.Idle;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (state == State.Flying && other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            rb.velocity = Vector2.zero;
+            state = State.Attached;
+        }
+    }
+    
+    public enum State
+    {
+        Attached,
+        Idle,
+        Flying
+    }
+}
---
+++ Assets/Scripts/GrapplingBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class GrapplingBase: MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    public GrapplingArm arm;
+    private float _armLength;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _armLength = Vector2.Distance(arm.transform.position, transform.position);
+    }
+
+    public void Update()
+    {
+        _lineRenderer.SetPosition(1, transform.position - arm.transform.position);
+        if (arm.state != GrapplingArm.State.Idle)
+        {
+            transform.up = arm.transform.position - transform.position;
+        }
+        else
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            transform.up = mousePosition - transform.position;
+        }
+
+        if (arm.state == GrapplingArm.State.Idle)
+        {
+            arm.transform.position = transform.up * _armLength + transform.position;
+        }
+
+        arm.transform.rotation = transform.rotation;
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            arm.Shoot();
+        }
+
+        if (Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            arm.Detach();
+        }
+    }
+}
