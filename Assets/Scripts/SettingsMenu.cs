c556878340dadda530bebc55a11a7deabd5d530a
maxivitop <max007vit@gmail.com>
2024-08-11 13:12:22 +0300

    menu and volume

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider masterVolumeSlider;
+    public Slider musicSlider;
+    public Slider SFXSlider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetMasterVolume(){
+        PlayerPrefs.SetFloat("masterVolume" , masterVolumeSlider.value);
+    }
+    public void SetMusicVolume(){
+        PlayerPrefs.SetFloat("musicVolume" , musicSlider.value);
+    }
+    public void SetSFXVolume(){
+        PlayerPrefs.SetFloat("SFXVolume" , SFXSlider.value);
+    }
+    public void BackToGame(){
+        Time.timeScale = 1.0f;
+        gameObject.SetActive(false);
     }
 }
