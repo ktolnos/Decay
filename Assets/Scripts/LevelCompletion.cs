d77e77191e2530bbec6d76c758ba8da5150a1a87
eop <ktolnos@mail.com>
2024-08-11 12:41:06 +0300

    Level completion

---
+++ Assets/Scripts/LevelCompletion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompletion: MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponentInParent<Player>() != null)
+        {
+            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1)
+                                   % SceneManager.sceneCountInBuildSettings);
+        }
+    }
+}
